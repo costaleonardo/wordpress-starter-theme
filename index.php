7c5cb3a6eacb7ecd03a31980ed586c4a0f60edda
costaleonardo <costaleonardo97@gmail.com>
2018-09-22 20:56:12 -0400

    Add posts display.

--- index.php
+++ index.php
@@ -1,12 +1,18 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="<?php bloginfo('charset'); ?>">
-  <title><?php bloginfo('name'); ?></title>
-  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>">
-  <?php wp_head(); ?>
-</head>
-<body>
+<?php get_header(); ?>
+	<div class="main">
+		<div class="container">
+			<?php if(have_posts()) : ?>
+				<?php while(have_posts()): the_post(); ?>
+					<h3><?php the_title(); ?></h3>
+					<div class="meta">
+					Created By <?php the_author(); ?> on <?php the_time('F j, Y g:i a'); ?>
+					</div>
+					<?php the_content(); ?>
+				<?php endwhile; ?>
+			<?php else : ?>
+				<?php echo wpautop('Sorry, No posts were found'); ?>
+			<?php endif; ?>
+		</div>
+	</div>
 
-</body>
-</html>
+<?php get_footer(); ?>
