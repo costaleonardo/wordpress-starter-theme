3e999133e23ef1311e9a90f124b897b0913c8d46
costaleonardo <costaleonardo97@gmail.com>
2018-09-22 20:24:54 -0400

    Push initial files.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <title><?php bloginfo('name'); ?></title>
+  <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>">
+  <?php wp_head(); ?>
+</head>
+<body>
+
+</body>
+</html>
